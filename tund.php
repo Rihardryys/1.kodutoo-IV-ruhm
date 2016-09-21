c745f7bf6cba6e5de920117bc2934136bed6676f
riharyys <riharyys@greeny.cs.tlu.ee>
2016-09-21 15:29:21 +0300

    add exisiting file

--- tund.php
+++ tund.php
@@ -1,8 +1,13 @@
 <?php
 //var_dump($_GET);
 //echo "<br>";
-var_dump($_POST);
 // $_post["signupEmail"] //annab muutuja v on muutuja
+//muutujad
+// <?php echo on sama mis <?=$m
+$signupEmailError = "";
+$signupEmail = "";
+
+
 $signupEmailError = "";
 
 // kas keegi vajutas nuppu ja see on olemas
@@ -12,23 +17,59 @@ if (isset ($_POST["signupEmail"])) {
 if(empty ($_POST["signupEmail"])){
 	// on tyhi
 	$signupEmailError="vali on kohustlik";
-}	
+}
+	else{ 
+	$signupEmail = $_POST["signupEmail"];
+}
 }
 
-var_dump($_POST);
 $signupPasswordError= "";
 if(isset($_POST["signupPassword"])){
 if (empty ($_POST["signupPassword"]))
+
 {
 	$signupPasswordError="vali on kohustuslik";
 	
-	
-	
+}
+
+}
+$loginPasswordError= "";
+if(isset($_POST["loginPassword"])){
+if (empty ($_POST["loginPassword"]))
+{
+	$loginPasswordError="vali on kohustuslik";
 	
 }
 
 }
+$loginEmailError= "";
+if(isset($_POST["loginEmail"])){
+if (empty ($_POST["loginEmail"]))
+{
+	$loginEmailError="väli on kohustuslik";
+	
+}
 
+}
+$male_status = 'unchecked';
+$female_status = 'unchecked';
+
+if (isset($_POST['Submit1'])) {
+
+$selected_radio = $_POST['gender'];
+
+if ($selected_radio = 'male') {
+
+$male_status = 'checked';
+
+}
+else if ($selected_radio = 'female') {
+
+$female_status = 'checked';
+
+}
+
+}
 ?>
 
 
@@ -43,18 +84,29 @@ if (empty ($_POST["signupPassword"]))
 	<body>
 	<h1> Logi sisse </h1>
 		<form method="POST">
-			<input type="Email" name="loginEmail" placeholder="naide@naide.ee"><br>
-			<input type="password" name="loginpassword" placeholder="parool"><br>
+			<input type="Email" name="loginEmail" placeholder="naide@naide.ee"><?php echo $loginEmailError; ?><br>
+			<input type="password" name="loginpassword" placeholder="parool"><?php echo $loginPasswordError; ?><br>
 
 			<input type="submit" value="vajuta">
 		</form>
 		<h1> Loo kasutaja </h1>
 		<form method="POST">
-			<input type="Email" name="signupEmail" placeholder="naide@naide.ee"><?php echo $signupEmailError; ?><br>
+			<input type="Email" name="signupEmail" placeholder="naide@naide.ee" value="<?=$signupEmail;?>"><?php echo $signupEmailError; ?><br>
 			<input type="password" name="signupPassword" placeholder="parool"><?php echo $signupPasswordError; ?><br>
 
 			<input type="submit" value="vajuta">
 		</form>
-	<body>
+		<FORM name ="form1" method ="post" action ="radioButton.php">
+
+		<Input type = 'Radio' Name ='gender' value= 'male'
+		<?PHP print $male_status; ?>
+		>Male
+
+		<Input type = 'Radio' Name ='gender' value= 'female' 
+		<?PHP print $female_status; ?>
+		>Female
+
+		</FORM>
+	</body>
 
 </html>
