03e5b553bde92aae3f13b3f16722dd70ddbe296e
riharyys <riharyys@greeny.cs.tlu.ee>
2016-09-28 14:09:54 +0300

    update

--- tund.php
+++ tund.php
@@ -1,77 +1,110 @@
-<?php
-//var_dump($_GET);
-//echo "<br>";
-// $_post["signupEmail"] //annab muutuja v on muutuja
-//muutujad
-// <?php echo on sama mis <?=$m
-$signupEmailError = "";
-$signupEmail = "";
-
-
-$signupEmailError = "";
-
-// kas keegi vajutas nuppu ja see on olemas
-if (isset ($_POST["signupEmail"])) {
-//on olemas
-//kas epost on tyhi
-if(empty ($_POST["signupEmail"])){
-	// on tyhi
-	$signupEmailError="vali on kohustlik";
-}
-	else{ 
-	$signupEmail = $_POST["signupEmail"];
-}
-}
-
-$signupPasswordError= "";
-if(isset($_POST["signupPassword"])){
-if (empty ($_POST["signupPassword"]))
-
-{
-	$signupPasswordError="vali on kohustuslik";
+<?php 
+	require("config.php");
+	echo $serverPassword;
+	//var_dump($_GET);
 	
-}
-
-}
-$loginPasswordError= "";
-if(isset($_POST["loginPassword"])){
-if (empty ($_POST["loginPassword"]))
-{
-	$loginPasswordError="vali on kohustuslik";
+	//echo "<br>";
 	
-}
-
-}
-$loginEmailError= "";
-if(isset($_POST["loginEmail"])){
-if (empty ($_POST["loginEmail"]))
-{
-	$loginEmailError="väli on kohustuslik";
+	//var_dump($_POST);
 	
-}
+	//MUUTUJAD
+	$signupEmailError = "";
+	$signupEmail = "";
+	
+	//kas email on olemas
+	
+	if (isset ($_POST["signupEmail"])) {
+		
+		//on olemas
+		// kas emaili väli on tühi
+		if (empty ($_POST["signupEmail"])) {
+			
+			// kui on tühi
+			$signupEmailError = " Väli on kohustuslik!";
+			
+		} else {
+			// email on olemas ja õige
+			$signupEmail = $_POST["signupEmail"];
+			
+		}
+		
+	} 
+	
+	$signupPasswordError = "";
+	
+	if (isset ($_POST["signupPassword"])) {
+		
+		if (empty ($_POST["signupPassword"])) {
+			
+			$signupPasswordError = "Väli on kohustuslik!";
+			
+		} else {
+			
+			// parool ei olnud tühi
+			
+			if ( strlen($_POST["signupPassword"]) < 8 ) {
+			
+				$signupPasswordError = "Parool peab olema vähemalt 8 tähemärkki pikk!";
+				
+			}
+			
+		}
+		
+	}
+	
+	if ( $signupEmailError == "" AND
+		$signupPasswordError == "" &&
+		isset ($_POST["signupEmail"]) &&
+		isset($_POST["signupPassword"])
+	//viga ei olnud, kõik väljad on täidetud (&&)
+	
+	){
+		echo "salvestan...<br>";
+		echo "email ".$signupEmail."<br>";
+		echo "parool".$_POST["signupPassword"]."<br>";
+		
+		$password = hash("sha512",$_POST["signupPassword"]);
+		
+		echo $password."<br>";
+		//loon ühenduse andembaasi
+		$database = "php1"; 		// Vaata üle databaasi nimi, täida ära, tee tabelid tund 3 järgi "" vahele.
+		
+		$mysqli = new mysqli($serverHost, $serverPassword, $serverUsername, $database);
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
+		echo $mysqli->error; //  kas töötab or nah ei tea
+		//asendan küsimärgid
+		//iga märgi kohta tuleb lisada üks täht - mis muutuja on
+		// s-string
+		// i- int
+		// d - double
+		$stmt->bind_param("ss", $signupEmail, $password);
+		if($stmt->execute() ) {
+			echo "õnnestus";
+		}else{"ERROR".$stmt->error;
+			
+			
+		}
+	
+	
+	
+	
+	
+	
+	}
+	//vaikimisi väärtus, radio buttonid
+	$gender = "";
+	
+	if (isset ($_POST["gender"])) {
+		if (empty ($_POST["gender"])) {
+			$genderError = "* Väli on kohustuslik!";
+		} else {
+			$gender = $_POST["gender"];
+		}
+		
+	} 
 
-}
-$male_status = 'unchecked';
-$female_status = 'unchecked';
 
-if (isset($_POST['Submit1'])) {
-
-$selected_radio = $_POST['gender'];
-
-if ($selected_radio = 'male') {
-
-$male_status = 'checked';
-
-}
-else if ($selected_radio = 'female') {
-
-$female_status = 'checked';
-
-}
-
-}
 ?>
-
 
 
 <!DOCTYPE html>
@@ -82,31 +115,50 @@ $female_status = 'checked';
 	</head>
 
 	<body>
-	<h1> Logi sisse </h1>
-		<form method="POST">
-			<input type="Email" name="loginEmail" placeholder="naide@naide.ee"><?php echo $loginEmailError; ?><br>
-			<input type="password" name="loginpassword" placeholder="parool"><?php echo $loginPasswordError; ?><br>
 
-			<input type="submit" value="vajuta">
+		<h1>Logi sisse</h1>
+		
+		<form method="POST" >
+			
+			<label>E-post</label><br>
+			<input name="loginEmail" type="email">
+			
+			<br><br>
+
+			<input name="loginPassword" placeholder="Parool" type="password">
+			
+			<br><br>
+			
+			<input type="submit" value="Logi sisse">	
+		
+		
+		<h1>Loo kasutaja</h1
+			
+			<label>E-post</label><br>
+			<input name="signupEmail" type="email" value="<?=$signupEmail;?>"> <?php echo $signupEmailError; ?>
+			
+			<br><br>
+
+			<input name="signupPassword" placeholder="Parool" type="password"> <?php echo $signupPasswordError; ?>
+			
+			<br><br>
+					
+			<?php if ($gender == "female") { ?>
+				<input type="radio" name="gender" value="female" checked> female<br>
+			<?php } else { ?>
+				<input type="radio" name="gender" value="female" > female<br>
+			<?php } ?>
+			
+			<?php if ($gender == "male") { ?>
+				<input type="radio" name="gender" value="male" checked> male<br>
+			<?php } else { ?>
+				<input type="radio" name="gender" value="male" > male<br>
+			<?php } ?>
+			
+			<input type="submit" value="Loo kasutaja">
+		
 		</form>
-		<h1> Loo kasutaja </h1>
-		<form method="POST">
-			<input type="Email" name="signupEmail" placeholder="naide@naide.ee" value="<?=$signupEmail;?>"><?php echo $signupEmailError; ?><br>
-			<input type="password" name="signupPassword" placeholder="parool"><?php echo $signupPasswordError; ?><br>
 
-			<input type="submit" value="vajuta">
-		</form>
-		<FORM name ="form1" method ="post" action ="radioButton.php">
-
-		<Input type = 'Radio' Name ='gender' value= 'male'
-		<?PHP print $male_status; ?>
-		>Male
-
-		<Input type = 'Radio' Name ='gender' value= 'female' 
-		<?PHP print $female_status; ?>
-		>Female
-
-		</FORM>
 	</body>
 
 </html>
